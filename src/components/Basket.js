830fa0f6630d8b504703ae2718793d039aef2e68
furkanjp <demirtasfurkan911@gmail.com>
2022-05-28 17:03:11 +0300

    content update

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -6,7 +6,7 @@ function Basket({ basket, resetBasket, total, products }) {
     return (
         <>
             <div className="basket-container container">
-                <h3>Alısverış Detayları</h3>
+                <h3>Alışveriş Detayları</h3>
                 <ul>
                     {basket.map(item => (
                         <BasketItem key={item.id} item={item} product={products.find(p => p.id === item.id)} />
